5c648202de11d58ff6ba9dcb9b6c76c5febbb546
lam33194 <lamntph33194@fpt.edu.vn>
2025-02-26 21:36:28 +0700

    feat(auth): add signup/login API using Sanctum

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginRequest;
+use App\Http\Requests\V1\RegisterRequest;
+use App\Models\User;
+use App\Traits\ApiResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use ApiResponse;
+    public function register(RegisterRequest $request)
+    {
+        $validatedData = $request->only(['email', 'password', 'name']);
+
+        $user = User::create($validatedData);
+
+        return $this->created('Đăng ký thành công', [
+            'user'  => $user,
+            'token' => $user->createToken('token-register')->plainTextToken
+        ]);
+    }
+    public function login(LoginRequest $request)
+    {
+        $credential = $request->only(['email', 'password']);
+
+        if (!Auth::attempt($credential)) {
+            return $this->unauthorize('Thông tin đăng nhập không chính xác');
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->ok('Đăng nhập thành công', [
+            'user'  => $user,
+            'token' => $user->createToken('token-login')->plainTextToken
+        ]);
+    }
+    public function logout() {}
+}
