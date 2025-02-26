12ef2226edcf56d71e6e3546e00fdfc8e32343fa
lam33194 <lamntph33194@fpt.edu.vn>
2025-02-26 18:12:21 +0700

    feat(auth): create request classes for login and register

---
+++ app/Http/Requests/V1/LoginRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // name
+        // email
+        // password
+        // phone
+        // address
+        // avatar
+        // role
+        return [
+            'email'     => 'required|email|unique:users,email',
+            'password'  => 'required'
+        ];
+    }
+
+    public function messages(): array {
+        return [ 
+            'email.required'    => 'Trường email là bắt buộc',
+            'email.email'       => 'Email không hợp lệ',  
+            'password.required' => 'Vui lòng nhập mật khẩu',
+        ];
+    }
+}
