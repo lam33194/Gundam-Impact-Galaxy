fb48ad4c8e85a0522bb113d6ed27c52d0071a167
Van Tung <tungvvph34095@fpt.edu.vn>
2025-03-29 23:43:07 +0700

    Fix : Update model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -28,37 +28,7 @@ class Order extends Model
         'pay_delivery'      => "Thanh toán khi nhận hàng"
     ];
 
-    public const STATUS_ORDER_PENDING = 'pending';
-    public const STATUS_ORDER_CONFIRMED = 'confirmed';
-    public const STATUS_ORDER_PREPARING_GOODS = 'preparing_goods';
-    public const STATUS_ORDER_SHIPPING = 'shipping';
-    public const STATUS_ORDER_DELIVERED = 'delivered';
-    public const STATUS_ORDER_CANCELED = 'canceled';
-    public const STATUS_PAYMENT_UNPAID = 'unpaid';
-    public const STATUS_PAYMENT_PAID = 'paid';
-    public const TYPE_PAYMENT_VNPAY = 'vnpay';
-    public const TYPE_PAYMENT_MOMO = 'momo';
-    public const TYPE_PAYMENT_PAY_DELIVERY = 'pay_delivery';
-
-    protected $fillable = [
-        'user_id',
-        'user_name',
-        'user_email',
-        'user_phone',
-        'user_address',
-        'user_note',
-        'same_as_buyer',
-        'ship_user_name',
-        'ship_user_email',
-        'ship_user_phone',
-        'ship_user_address',
-        'ship_user_note',
-        'status_order',
-        'status_payment',
-        'type_payment',
-        'total_price',
-        'order_sku'
-    ];
+   
 
     public function orderItems()
     {
