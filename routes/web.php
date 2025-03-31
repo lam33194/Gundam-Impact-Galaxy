263e4cd2243a54431222966556fe882a436b07e1
lam33194 <lamntph33194@fpt.edu.vn>
2025-03-31 14:29:02 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/{any}', function () {
+//     return view('index');
+// })->where('any', '^(?!admin).*');
+
 Route::get('/', function () {
     return redirect()->route('admin.dashboard');
 });
@@ -22,7 +26,3 @@ Route::get('/login', [LoginController::class, 'showFormLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::post('/logout', [LoginController::class, 'logout']);
-
-// Route::get('/{any}', function () {
-//     return view('index');
-// })->where('any', '.*');
