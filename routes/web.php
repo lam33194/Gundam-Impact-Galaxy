aaaf32c83129f7f8734f0c391adfa4033f52dfce
Stussty <nhoksaker2k4@gmail.com>
2025-04-01 08:36:39 +0700

    add Admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return view('index');
 })->where('any', '^(?!admin).*');
+
+Route::prefix('admin')->group(function() {
+    Route::view('/', 'admin.dashboard')->name('dashboard');
+});
