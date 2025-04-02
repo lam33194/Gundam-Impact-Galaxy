304e266eac6b16b982195b245a23870a55d6758f
Stussty <nhoksaker2k4@gmail.com>
2025-04-02 22:41:40 +0700

    fix db tag

--- database/migrations/2025_03_30_084410_tags_table.php
+++ database/migrations/2025_03_30_084410_tags_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255)->unique();
-            $table->string('slug')->unique();
+            // $table->string(column: 'slug')->unique();
             $table->timestamps();
         });
     }
