8e54ee3b6feaecd591801d01ff1db239a45ec83a
Van Tung <tungvvph34095@fpt.edu.vn>
2025-04-03 22:13:12 +0700

    update category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -37,5 +37,41 @@ class CategoryController extends Controller
     {
         return view(self::PATH_VIEW . __FUNCTION__);
     }
+    public function store(StoreCategoryRequest $request)
+    {
+        try {
+            $this->categoryService->store($request->all());
+
+            Toastr::success(null, 'Thao tác thành công');
+            return redirect()->route('admin.categories.index');
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+        }
+    }
+
+    public function show(Category $category)
+    {
+        return view(self::PATH_VIEW . __FUNCTION__, compact('category'));
+    }
+
+    public function edit(Category $category)
+    {
+        return view(self::PATH_VIEW . __FUNCTION__, compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        try {
+            $this->categoryService->update($category, $request->all());
+
+            Toastr::success(null, 'Thao tác thành công');
+            return redirect()->back();
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+        }
+    }
   
 }
