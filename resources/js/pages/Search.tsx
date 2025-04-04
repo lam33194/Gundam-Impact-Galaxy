b825c7194b266802a511c7bed95f355f96105ed3
tuan <tuanbnph47980@fpt.edu.vn>
2025-04-05 00:37:35 +0700

    ghep API lọc theo loại và theo giá

--- resources/js/pages/Search.tsx
+++ resources/js/pages/Search.tsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import Product from "../components/Product";
 import "./Search.scss";
 import { useNavigate } from "react-router-dom";
-import { getAll } from "../services/ProductService";
+import { getAll, getAllByCategory } from "../services/ProductService";
+import { fetchAll } from "../services/CategoryService";
+import { FormatCurrency } from "../utils/FormatCurrency";
 
 function Search() {
+    const [priceRange, setPriceRange] = useState({ min: "", max: "" });
+    const [selectedType, setSelectedType] = useState('');
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [keyword, setKeyword] = useState("");
+    const [filterTitle, setFilterTitle] = useState('');
     const navigate = useNavigate();
 
     const getAllProducts = async () => {
@@ -20,43 +27,136 @@ function Search() {
         }
     };
 
+    const getAllCategories = async () => {
+        try {
+            const res = await fetchAll();
+            if (res.data && res.data.data) {
+                console.log(res.data.data);
+                setCategories(res.data.data);
+            }
+        } catch (error) {
+            console.log("Detected error:", error);
+        }
+    };
+
     const redirectToDetail = (slug: any) => {
         navigate("/product/" + slug);
     };
 
+    const handlePriceChange = (e: any) => {
+        const { name, value } = e.target;
+        setPriceRange({ ...priceRange, [name]: value });
+    };
+
+    const handleTypeChange = async (type: any) => {
+       
+        const res = await getAllByCategory(type);
+        setProducts(res.data.data);
+        setFilterTitle("Lọc theo loại: " + type);
+        setSelectedType(type);
+    };
+    
+
+    const applyFilters = async() => {
+        console.log("Min Price:", priceRange.min);
+        console.log("Max Price:", priceRange.max);
+        console.log("Selected Types:", selectedType);
+        setSelectedType('')
+        try {
+            const params = {
+                min_price: priceRange.min,
+                max_price: priceRange.max,
+            }
+            const res = await getAll(params);
+            if (res.data && res.data.data) {
+                console.log(res.data.data);
+                setProducts(res.data.data);
+                setFilterTitle("Lọc theo giá: " + FormatCurrency(priceRange.min) + 'đ - ' + FormatCurrency(priceRange.max) + 'đ');
+            }
+        } catch (error) {
+            console.log("Detected error:", error);
+        }
+    };
+
     useEffect(() => {
         getAllProducts();
+        getAllCategories();
     }, []);
     return (
         <div className="container mt-5">
             <h1 className="text-center mb-4">Tìm kiếm sản phẩm</h1>
-
-            {/* Thanh tìm kiếm */}
             <div className="input-group mb-4">
                 <input
                     type="text"
                     className="form-control"
                     placeholder="Nhập tên sản phẩm..."
+                    style={{ borderRadius: '0.25rem 0 0 0.25rem' }}
                 />
-                <button className="btn btn-primary" type="button">
+                <button className="btn btn-primary" type="button" style={{ borderRadius: '0 0.25rem 0.25rem 0' }}>
                     Search
                 </button>
             </div>
 
-            <h5 className="mt-4">DANH SÁCH SẢN PHẨM</h5>
-            <ul className="list-group mt-3">
-                {products &&
-                    products.map((p, index) => {
-                        return (
-                            <div
-                                className="prod"
-                                onClick={() => redirectToDetail(p.slug)}
-                            >
-                                <Product key={index} p={p} />
+            <div className="row">
+                <div className="col-lg-3 mt-5">
+                    <div className="card p-3 shadow-sm">
+                        <h4 className="mb-3">Mức Giá</h4>
+                        <div className="d-flex mb-3">
+                            <input
+                                type="number"
+                                name="min"
+                                className="form-control me-2"
+                                placeholder="Từ"
+                                value={priceRange.min}
+                                onChange={handlePriceChange}
+                            />
+                            <input
+                                type="number"
+                                name="max"
+                                className="form-control"
+                                placeholder="Đến"
+                                value={priceRange.max}
+                                onChange={handlePriceChange}
+                            />
+                        </div>
+                        <button className="btn btn-dark mb-4 w-100" onClick={applyFilters}>
+                            Áp Dụng Giá
+                        </button>
+
+                        <h4 className="mb-3">Loại</h4>
+                        {categories.map((c, index) => (
+                            <div className="form-check mb-2" key={index}>
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    checked={selectedType === c.slug}
+                                    onChange={() => handleTypeChange(c.slug)}
+                                />
+                                <label className="form-check-label">
+                                    {c.name}
+                                </label>
                             </div>
-                        );
-                    })}
-            </ul>
+                        ))}
+                    </div>
+                </div>
+
+                <div className="col-lg-8 offset-lg-1">
+                    <h5 className="mt-3">DANH SÁCH SẢN PHẨM</h5>
+                    <span className="mt-3 font-italic">{filterTitle}</span>
+                    <ul className="list-group mt-3">
+                        {products &&
+                            products.map((p, index) => (
+                                <li
+                                    key={index}
+                                    className="list-group-item list-group-item-action"
+                                    onClick={() => redirectToDetail(p.slug)}
+                                >
+                                    <Product p={p} />
+                                </li>
+                            ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
--- resources/js/services/ProductService.tsx
+++ resources/js/services/ProductService.tsx
@@ -1,7 +1,12 @@
 import customizeAxios from './customize-axios';
-export const getAll = (page?: any, size?: any): Promise<any> =>{
-    return customizeAxios.get('/api/v1/products');
-}
+export const getAll = (params?: any): Promise<any> => {
+    return customizeAxios.get('/api/v1/products', { params });
+};
+
+export const getAllByCategory = (category_slug: any): Promise<any> => {
+    return customizeAxios.get(`/api/v1/categories/${category_slug}/products`);
+};
+
 
 export const getDetail = (slug: any): Promise<any> =>{
     return customizeAxios.get(`/api/v1/products/${slug}?include=category,variants`);
