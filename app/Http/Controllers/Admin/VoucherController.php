2b7d00c13d0e96a78ed5837ede57e893c29957f4
Van Tung <tungvvph34095@fpt.edu.vn>
2025-04-09 11:48:53 +0700

    logic voucher

--- app/Http/Controllers/Admin/VoucherController.php
+++ app/Http/Controllers/Admin/VoucherController.php
@@ -2,61 +2,30 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helper\Toastr;
 use App\Http\Controllers\Controller;
-use App\Models\Voucher;
 use Illuminate\Http\Request;
+use App\Models\Voucher;
+use Illuminate\Support\Str;
+use App\Http\Requests\VoucherRequest;
 
 class VoucherController extends Controller
 {
-    public function index()
+    private const PATH_VIEW = 'admin.vouchers.';
+    public function index(Request $request)
     {
-        $vouchers = Voucher::paginate(10);
+        $search = $request->input('search');
 
-        return view('admin.vouchers.index', compact('vouchers'));
+        $vouchers = Voucher::when($search, function ($query, $search) {
+
+            return $query->where('code', 'like', '%' . $search . '%');
+        })->latest('id')->paginate(5);
+
+        return view(self::PATH_VIEW . __FUNCTION__, compact('vouchers', 'search'));
     }
 
     public function create()
     {
-        //
-    }
-
-    public function store(Request $request)
-    {
-        //
-    }
-
-    public function show(string $id)
-    {
-        //
-    }
-
-    public function edit(string $id)
-    {
-        //
-    }
-
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    public function destroy(string $id)
-    {
-        //
-    }
-    
-    public function toggleStatus($id, Request $request)
-    {
-        try {
-            $voucher = Voucher::findOrFail($id);
-
-            $voucher->is_active = $request->has('is_active');
-
-            $voucher->save();
-
-            return back()->with('success', 'Thao tác thành công !!!');
-        } catch (\Throwable $th) {
-            return back()->with('error', 'Thao tác không thành công !!!');
-        }
+        return view(self::PATH_VIEW . __FUNCTION__);
     }
 }
---
+++ app/Http/Requests/VoucherRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VoucherRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     */
+    public function rules(): array
+    {
+        if ($this->isMethod('post')) {
+            return $this->rulesForCreate();
+        } elseif ($this->isMethod('put') || $this->isMethod('patch')) {
+            return $this->rulesForUpdate();
+        }
+
+        return [];
+    }
+}
