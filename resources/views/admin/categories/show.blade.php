b1d60b964ea6b2a3d301cc6588c83e284b35c70f
Stussty <nhoksaker2k4@gmail.com>
2025-05-03 13:09:35 +0700

    Update tieng viet

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -7,14 +7,14 @@
 <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-            <h4 class="mb-sm-0 font-size-18">Show Category: {{ $category->name }}</h4>
+            <h4 class="mb-sm-0 font-size-18">Chi Tiết Danh Mục : {{ $category->name }}</h4>
 
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item">
-                        <a href="{{ route('admin.categories.index') }}">Categories</a>
+                        <a href="{{ route('admin.categories.index') }}">Danh Mục</a>
                     </li>
-                    <li class="breadcrumb-item active">Show Category</li>
+                    <li class="breadcrumb-item active">Chi Tiết Danh Mục </li>
                 </ol>
             </div>
         </div>
@@ -26,7 +26,7 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="mb-3">
-                                <label for="projectname-input" class="form-label">Name</label>
+                                <label for="projectname-input" class="form-label">Tên</label>
                                 <input id="projectname-input" name="name" type="text" class="form-control"
                                     placeholder="Enter category name..." value="{{ $category->name }}" disabled
                                     required>
@@ -40,7 +40,7 @@
                             </div>
 
                             <div class="mb-3">
-                                <label for="projectname-input" class="form-label">Description</label>
+                                <label for="projectname-input" class="form-label">Mô tả</label>
                                 <textarea name="description" type="text" class="form-control"
                                     placeholder="Enter category description..." disabled required>
                                     {{ $category->description }}
@@ -54,7 +54,7 @@
                 <div class="col-lg-4">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title mb-3">Publish</h5>
+                            <h5 class="card-title mb-3">Trạng thái</h5>
 
                             <div class="mb-3">
                                 <div class="form-check form-switch mb-3">
